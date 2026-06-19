c983bb92c697654e61ce2d18973a0beb3ea3db78
KRYA-Official <krya.global@gmail.com>
2026-06-19 13:28:05 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Firebase कॉन्फ़िगरेशन
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "KRYA-MASTER.firebaseapp.com",
+    projectId: "KRYA-MASTER",
+    storageBucket: "KRYA-MASTER.appspot.com",
+    messagingSenderId: "YOUR_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// नोट: जब आप Firebase कंसोल पर जाएंगे, तो आपको ये डिटेल्स मिलेंगी।
+// उन्हें यहाँ डालकर रिप्लेस कर देना।
+console.log("KRYA Firebase Service Initialized");
+
